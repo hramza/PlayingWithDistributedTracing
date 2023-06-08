696e7fbecb791e4b3f60d5cc2a04079e50001915
Hamza RAMZA <ramza.hamza.dev@gmail.com>
2023-06-08 22:55:14 +0200

    Updated log message

--- src/SearchEngine.Shared/Log.cs
+++ src/SearchEngine.Shared/Log.cs
@@ -13,7 +13,7 @@ namespace SearchEngine.Shared
         [LoggerMessage(
             EventId = 1,
             Level = LogLevel.Information,
-            Message = "Consumed a record")]
+            Message = "Consuming a record")]
         public static partial void ConsumedARecord(this ILogger logger);
 
         [LoggerMessage(
